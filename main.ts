57d23c7622e737d37fbdbc893ebbf2852bc418b5
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-02 15:43:56 -0400

    adding sound

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 let timer = 0
 input.onButtonPressed(Button.A, function () {
     timer = randint(5, 15)
-    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.Square)
     while (0 < timer) {
         timer += -1
+        music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Forever)
         basic.showIcon(IconNames.SmallDiamond)
-        basic.showIcon(IconNames.Target)
+        basic.showIcon(IconNames.Diamond)
         basic.pause(1000)
     }
-    music.playMelody("- - - D D - - - ", 120)
+    music.stopAllSounds()
     basic.showIcon(IconNames.Skull)
 })
 basic.forever(function () {
