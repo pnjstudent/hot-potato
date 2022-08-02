a6ac14dd11a64378b3535527c989ca7ceee37ea2
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-02 14:48:15 -0400

    adding flashing icon

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Diamond)
     while (0 < timer) {
         timer += -1
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.Target)
         basic.pause(1000)
     }
-    basic.showIcon(IconNames.Skull)
     music.playMelody("- - - D D - - - ", 120)
+    basic.showIcon(IconNames.Skull)
 })
 basic.forever(function () {
 	
